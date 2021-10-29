c4a681359820d5f3b6725a5d9d97f164d0b3fe42
huangmickey <67403573+huangmickey@users.noreply.github.com>
2021-10-28 23:00:14 -0700

    Adding all files

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings() {
+
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+
+}
